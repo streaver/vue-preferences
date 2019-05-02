589972da7eec30006b291a49bea1e5b265cb0252
Federico Kauffman <federico@streaver.com>
2019-05-01 21:03:35 -0300

    Test reactive preferences when using mapPreferences

--- tests/unit/map-preferences.spec.js
+++ tests/unit/map-preferences.spec.js
@@ -1,4 +1,9 @@
-import { mapPreferences, DEFAULT_STORAGE_PREFIX } from '../../src/index';
+import setupVue from '../utils/setup-vue';
+import {
+  mapPreferences,
+  DEFAULT_STORAGE_PREFIX,
+  DEFAULT_REACTIVE_PROPERTIES_PREFIX,
+} from '../../src/index';
 
 describe('VuePreferences#mapPreferences', () => {
   beforeEach(() => {
@@ -104,6 +109,42 @@ describe('VuePreferences#mapPreferences', () => {
           const defaultValue = preferencesObject[preferenceName].defaultValue;
 
           expect(preference.get(preferenceName)).toBe(defaultValue);
+        });
+      });
+
+      it('sets up reactivity for the preferences that require it', () => {
+        const preferencesObject = {
+          firstName: {},
+          lastName: {
+            reactive: true,
+          },
+        };
+
+        subject = mapPreferences(preferencesObject);
+
+        Object.keys(preferencesObject).forEach((preferenceName, index) => {
+          const preference = subject[preferenceName];
+          const reactive = preferencesObject[preferenceName].reactive;
+          const { wrapper, setSpy, getItemSpy } = setupVue(preference);
+
+          preference.set(`Alice - ${index}`);
+
+          expect(preference.get()).toBe(`Alice - ${index}`);
+
+          if (reactive) {
+            expect(setSpy).toHaveBeenCalledTimes(1);
+            expect(wrapper.vm[DEFAULT_REACTIVE_PROPERTIES_PREFIX]).toEqual({
+              [`${DEFAULT_STORAGE_PREFIX}:${preferenceName}`]: `Alice - ${index}`,
+            });
+            expect(getItemSpy).not.toHaveBeenCalled();
+          } else {
+            expect(setSpy).not.toHaveBeenCalled();
+            expect(wrapper.vm[DEFAULT_REACTIVE_PROPERTIES_PREFIX]).toEqual({});
+            expect(getItemSpy).toHaveBeenCalledTimes(1);
+          }
+
+          setSpy.mockRestore();
+          getItemSpy.mockRestore();
         });
       });
     });
