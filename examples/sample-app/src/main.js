913693b506a3823a6f36559f685801900d0b7203
Federico Kauffman <federico@streaver.com>
2019-05-07 17:48:08 -0300

    Run prettier on examples/sample-app/src/main.js

--- examples/sample-app/src/main.js
+++ examples/sample-app/src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue';
+import App from './App.vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
