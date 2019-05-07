b0e59fa43440d56dcee4806a2c9d2119860136fa
Federico Kauffman <federico@streaver.com>
2019-05-07 17:48:45 -0300

    Install vue-preferences on sample app

--- examples/sample-app/src/main.js
+++ examples/sample-app/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
+import VuePreferences from 'vue-preferences';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
+Vue.use(VuePreferences);
 
 Vue.config.productionTip = false;
 
